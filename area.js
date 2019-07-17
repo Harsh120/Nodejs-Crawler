8aa7a7a0b58cfc313a50b250173afd030e90c5cf
Harsh120 <harshitmahajan120@gmail.com>
2019-07-17 16:25:28 +0530

    Update area.js

--- area.js
+++ area.js
@@ -6,7 +6,7 @@ const $ = require('cheerio');
 const url = 'https://www.practo.com/sitemap/indore-listings/localities-listings/page-1';
 const split_city_name1 = url.split('https://www.practo.com/sitemap/'); 
 split_city_name2 = split_city_name1[1].split('/');
-city = split_city_name2[0];
+city = split_city_name2[0]; // To get city name
 scrap(url,city);
 const remain = [];
 var flag = 0;
@@ -23,13 +23,13 @@ function scrap(url,city)
         for (var i=0;i<200;i++) {
             try
             {
-                var a = ($('a', html)[i].attribs.href);
-                if(a.startsWith('/sitemap/'+city+'/'))
+                var a = ($('a', html)[i].attribs.href);  // get href
+                if(a.startsWith('/sitemap/'+city+'/'))    // if href is like /sitemap/agra-listings/MG-road
                 {
                     var fields = a.split(city+'/');
                     var area = fields[1];
                     console.log(area);
-                    if(area.startsWith('localities-listings'))
+                    if(area.startsWith('localities-listings'))  // if href is like /sitemap/agra-listings/localities-listings/page-2 then save in remain for another page
                     {
                         if(flag==0)
                         {
@@ -44,7 +44,7 @@ function scrap(url,city)
             }
             catch(err)
             {
-                console.log('Nothing');
+                console.log('Nothing');  // if area are less than number of loop then do nothing 
             }   
         }
         flag++;
@@ -62,7 +62,7 @@ function scrap(url,city)
 }
 
 
-function remaining(remain)
+function remaining(remain)  // To scrap other pagination pages
 {
     console.log(remain.length);
     for(var i=0;i<remain.length;i++)
